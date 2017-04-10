bd0892c282732141285e20157ef7db7554d9691c
Håkon K. Olafsen <hakon-klovstad.olafsen@ffi.no>
2017-04-10 21:38:56 +0200

    Init

---
+++ NetFramework/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace NetFramework
+{
+    public static class Class1
+    {
+        public static string Method(string someValue)
+        {
+            return $"A string with {someValue} embedded in it.";
+        }
+    }
+}
